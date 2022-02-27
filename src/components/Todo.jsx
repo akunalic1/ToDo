47a313979d8b6d393e27cf4ca2dce22e464cd37e
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-27 23:25:48 +0100

    Open comment implemented

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,10 +9,15 @@ import "./../css/todo.css";
 
 const Todo = ({ todo }) => {
   const [comment, setComment] = useState(false);
+  const [openComment, setOpenComent] = useState(false);
 
   useEffect(() => {
     setComment(todo.comment !== "");
   }, []);
+
+  const handleOpenComment = (e) => {
+    setOpenComent(!openComment);
+  };
 
   return (
     <div className={`todo-item glass ${todo.status}-border`}>
@@ -32,10 +37,19 @@ const Todo = ({ todo }) => {
         <button>
           <FontAwesomeIcon icon={faEdit}></FontAwesomeIcon>
         </button>
-        <button className="comment-btn">
-          <FontAwesomeIcon icon={faComment}></FontAwesomeIcon>
-          <div className={comment ? `comment-exist` : ""}></div>
-        </button>
+        <div className="comment-wrapper">
+          <button onClick={handleOpenComment} className="comment-btn">
+            <FontAwesomeIcon icon={faComment}></FontAwesomeIcon>
+            <div className={comment ? `comment-exist` : ""}></div>
+          </button>
+          <div
+            className={`comment-content ${
+              !openComment || !comment ? "hide" : ""
+            }`}
+          >
+            {todo.comment}
+          </div>
+        </div>
       </div>
     </div>
   );
