29935beebb1d6525f1ee090e66c75da4b7974668
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-27 12:03:39 +0100

    Todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Todo = () => {
-  return <div>Todo</div>;
+const Todo = ({ todo }) => {
+  console.log(todo);
+  return (
+    <div className={`todo-item glass ${todo.status}-border`}>
+      <p className="todo-title">{todo.title}</p>
+      <p className="todo-description">{todo.text}</p>
+      <p className="todo-date">{todo.createdAt}</p>
+    </div>
+  );
 };
 
 export default Todo;
