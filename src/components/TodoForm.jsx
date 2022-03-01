017a783ce761d1e882e577e72afdbe3a242d2be0
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-03-01 11:34:27 +0100

    TodoForm refactored

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -7,47 +7,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FilterTodo from "./FilterTodo";
 
-const TodoForm = ({
-  handleFilterList,
-  handleFilterOption,
-  handleShowHideInputs,
-  showAddTask,
-  showFilterList,
-}) => {
-  const renderForm = () => {
-    return (
-      <div className="fields">
-        <div className="fields-buttons">
-          <button
-            className={`btn add-task-btn glass ${!showAddTask ? "hide" : ""}`}
-            onClick={handleShowHideInputs}
-          >
-            <FontAwesomeIcon
-              icon={faPlus}
-              className="add-task-icon"
-            ></FontAwesomeIcon>
-            Add a task
-          </button>
-          <button
-            className={`btn btn-filter glass ${!showAddTask ? "hide" : ""}`}
-            onClick={handleFilterList}
-          >
-            Filter
-            <FontAwesomeIcon
-              icon={faAngleDown}
-              className="add-task-icon"
-            ></FontAwesomeIcon>
-          </button>
-          <div
-            className={`filter-options glass ${!showFilterList ? "hide" : ""}`}
-          >
-            <FilterTodo handleFilterOption={handleFilterOption} />
-          </div>
-        </div>
-        {renderInputFields()}
-      </div>
-    );
-  };
+const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
   const renderInputFields = () => {
     return (
       <div className={`create-todo glass ${showAddTask ? "hide" : ""}`}>
@@ -75,7 +35,7 @@ const TodoForm = ({
       </div>
     );
   };
-  return <>{renderForm()}</>;
+  return <>{renderInputFields()}</>;
 };
 
 export default TodoForm;
