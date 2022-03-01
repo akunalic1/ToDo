996f6c01d626a6f144677cb2ebfd61aeefde193e
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-03-01 12:20:25 +0100

    Clicked status indicator

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import {
   faClose,
   faPlus,
@@ -8,13 +8,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FilterTodo from "./FilterTodo";
 
 const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
+  const titleRef = createRef();
+  const descriptionRef = createRef();
+  let statusList = [];
+
+  const handleStatusClicked = (e) => {
+    if (statusList.includes(e.target.value)) {
+      e.target.classList.add("not-clicked");
+      const indexOfStatus = statusList.indexOf(e.target.value);
+      statusList.splice(indexOfStatus, 1);
+    } else {
+      e.target.classList.remove("not-clicked");
+      statusList.push(e.target.value);
+    }
+    console.log(statusList);
+  };
+
   const renderInputFields = () => {
     return (
       <div className={`create-todo glass ${showAddTask ? "hide" : ""}`}>
         <div className="create-todo-top">
           <div className="inputs">
-            <input className="create-title" placeholder="Task title..."></input>
             <input
+              ref={titleRef}
+              className="create-title"
+              placeholder="Task title..."
+            ></input>
+            <input
+              ref={descriptionRef}
               className="create-description"
               placeholder="Description..."
             ></input>
@@ -26,12 +47,30 @@ const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
         <p className="label">Set as:</p>
         <div className="create-status">
           <div className="status-btns">
-            <button className="btn urgent">urgent</button>
-            <button className="btn important">important</button>
-            <button className="btn not-priority">not priority</button>
+            <button
+              value={"urgent"}
+              onClick={handleStatusClicked}
+              className="btn urgent not-clicked"
+            >
+              urgent
+            </button>
+            <button
+              value={"important"}
+              onClick={handleStatusClicked}
+              className="btn important not-clicked"
+            >
+              important
+            </button>
+            <button
+              value={"not-a-priority"}
+              onClick={handleStatusClicked}
+              className="btn not-a-priority not-clicked"
+            >
+              not priority
+            </button>
           </div>
-          <div></div>
         </div>
+        <button className="btn glass btn-submit">Submit</button>
       </div>
     );
   };
