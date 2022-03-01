b5e5cf88700d4b8a65d97e2f110e443ef97687ea
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-03-01 17:47:33 +0100

    Create todo implemented

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React, { createRef, useEffect, useState } from "react";
 import {
   faClose,
   faPlus,
@@ -6,27 +6,74 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import FilterTodo from "./FilterTodo";
+import server from "../api/server";
 
 const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
   const titleRef = createRef();
   const descriptionRef = createRef();
+  const statusBtnsRef = createRef();
+  const formRef = createRef();
   let statusList = [];
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (!errorMessage.length && titleRef.current.value.length) {
+      const saveTodo = async () => {
+        const response = await server.post("/todos", {
+          title: titleRef.current.value,
+          text: descriptionRef.current.value,
+          createdAt: new Date().toDateString(),
+          status: statusList[0],
+          comment: "",
+          completed: false,
+        });
+
+        console.log(response);
+      };
+      saveTodo();
+    }
+  }, [errorMessage]);
 
   const handleStatusClicked = (e) => {
+    e.preventDefault();
     if (statusList.includes(e.target.value)) {
+      statusList.pop();
       e.target.classList.add("not-clicked");
-      const indexOfStatus = statusList.indexOf(e.target.value);
-      statusList.splice(indexOfStatus, 1);
     } else {
+      Array.from(statusBtnsRef.current.children).forEach((button) =>
+        button.classList.add("not-clicked")
+      );
       e.target.classList.remove("not-clicked");
       statusList.push(e.target.value);
     }
-    console.log(statusList);
   };
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (!titleRef.current.value || !titleRef.current.value.trim().length)
+      setErrorMessage("Title cannot be empty");
+    else {
+      const saveTodo = async () => {
+        const response = await server.post("/todos", {
+          title: titleRef.current.value,
+          text: descriptionRef.current.value,
+          createdAt: new Date().toLocaleDateString("en-US"),
+          status: statusList.length === 0 ? "default" : statusList[0],
+          comment: "",
+          completed: false,
+        });
 
+        console.log(response);
+      };
+      saveTodo();
+      formRef.current.reset();
+    }
+  };
   const renderInputFields = () => {
     return (
-      <div className={`create-todo glass ${showAddTask ? "hide" : ""}`}>
+      <form
+        ref={formRef}
+        className={`create-todo glass ${showAddTask ? "hide" : ""}`}
+      >
         <div className="create-todo-top">
           <div className="inputs">
             <input
@@ -46,7 +93,7 @@ const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
         </div>
         <p className="label">Set as:</p>
         <div className="create-status">
-          <div className="status-btns">
+          <div ref={statusBtnsRef} className="status-btns">
             <button
               value={"urgent"}
               onClick={handleStatusClicked}
@@ -70,8 +117,11 @@ const TodoForm = ({ handleShowHideInputs, showAddTask }) => {
             </button>
           </div>
         </div>
-        <button className="btn glass btn-submit">Submit</button>
-      </div>
+        <p className="error-message">{errorMessage}</p>
+        <button onClick={handleSubmitForm} className="btn glass btn-submit">
+          Submit
+        </button>
+      </form>
     );
   };
   return <>{renderInputFields()}</>;
